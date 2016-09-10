e3b17ea81d6be5a46d2b243f724adc093e86da9f
Ryoji Miyazaki <Ryoji@RS-MacBook-Air-Home.local>
2016-09-11 00:27:08 +0900

    Initial commit

---
+++ diarygram/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  diarygram
+//
+//  Created by Ryoji Miyazaki on 2016/09/11.
+//  Copyright © 2016年 Ryoji Miyazaki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
